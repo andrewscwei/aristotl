c71f10adeb0c1ec1de074c02d1c352b0cb454e06
Andrew Wei <andrewscwei@gmail.com>
2019-09-20 12:22:21 -0700

    Create Definition component

---
+++ src/components/Definition.tsx
@@ -0,0 +1,44 @@
+import { Document } from 'prismic-javascript/d.ts/documents';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { Action, bindActionCreators, Dispatch } from 'redux';
+import styled from 'styled-components';
+import { AppState } from '../store';
+import { I18nState } from '../store/i18n';
+
+interface StateProps {
+  i18n: I18nState;
+}
+
+interface DispatchProps {
+
+}
+
+interface Props extends StateProps, DispatchProps {
+  className?: string;
+  doc: Document;
+}
+
+class Definition extends PureComponent<Props> {
+  render() {
+    const { ltxt } = this.props.i18n;
+
+    return (
+      <StyledRoot className={this.props.className}>
+      </StyledRoot>
+    );
+  }
+}
+
+export default connect(
+  (state: AppState): StateProps => ({
+    i18n: state.i18n,
+  }),
+  (dispatch: Dispatch<Action>): DispatchProps => bindActionCreators({
+
+  }, dispatch),
+)(Definition);
+
+const StyledRoot = styled.button`
+
+`;
