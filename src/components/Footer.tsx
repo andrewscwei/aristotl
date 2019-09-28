0a71d451ff2eb4ec23f1c70b29c11fe34e0cf98e
Andrew Wei <andrewscwei@gmail.com>
2019-09-28 14:50:23 -0700

    Fix lint errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Document } from 'prismic-javascript/d.ts/documents';
-import { animations, selectors, container } from 'promptu';
+import { animations, container, selectors } from 'promptu';
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Action, bindActionCreators, Dispatch } from 'redux';
