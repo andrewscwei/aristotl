b37e309779977540bf50f6e84199ee5678839eb8
Andrew Wei <andrewscwei@gmail.com>
2019-09-27 21:07:38 -0700

    Prevent rerequesting of fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,9 @@
-import { normalize, typography } from 'promptu';
+import { normalize } from 'promptu';
 import { createGlobalStyle } from 'styled-components';
 import { colors } from './theme';
 
 export default createGlobalStyle`
   ${normalize()} /* stylelint-disable-line max-empty-lines */
-
-  ${typography.fontFace('RobotoMono', require('../assets/fonts/RobotoMono-Regular.ttf'), 400)}
-  ${typography.fontFace('RobotoMono', require('../assets/fonts/RobotoMono-RegularItalic.ttf'), 400)}
-  ${typography.fontFace('RobotoMono', require('../assets/fonts/RobotoMono-Bold.ttf'), 700)}
-  ${typography.fontFace('RobotoMono', require('../assets/fonts/RobotoMono-BoldItalic.ttf'), 700)}
-  ${typography.fontFace('NovaMono', require('../assets/fonts/NovaMono-Regular.ttf'), 400)}
 
   html,
   body {
