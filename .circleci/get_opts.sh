ef38952c5030c6863a8061a3466d1ba031018214
Andrew Wei <andrewscwei@gmail.com>
2019-09-28 14:52:43 -0700

    Remove CircleCI script

--- .circleci/get_opts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PACKAGE_NAME=$(cat package.json | jq -r ".name")
-PACKAGE_VERSION=$(cat package.json | jq -r ".version")
-PACKAGE_FILE=$PACKAGE_NAME-$PACKAGE_VERSION
-GIT_ORIGIN_URL=`git config --get remote.origin.url`
