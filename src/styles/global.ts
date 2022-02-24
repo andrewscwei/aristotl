7624fce3cd3647908764df3399ab1612555d0c39
Andrew Wei <andrewscwei@gmail.com>
2022-02-23 17:26:35 -0800

    Update fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,15 @@
-import { normalize } from 'promptu'
+import { normalize, typography } from 'promptu'
 import { css } from 'styled-components'
 import { colors } from './theme'
 
 export default css`
   ${normalize()} /* stylelint-disable-line max-empty-lines */
+
+  ${typography.fontFace('RobotoMono', '/fonts/RobotoMono-Regular.ttf')}
+  ${typography.fontFace('RobotoMono', '/fonts/RobotoMono-RegularItalic.ttf')}
+  ${typography.fontFace('RobotoMono', '/fonts/RobotoMono-Bold.ttf')}
+  ${typography.fontFace('RobotoMono', '/fonts/RobotoMono-BoldItalic.ttf')}
+  ${typography.fontFace('NovaMono', '/fonts/NovaMono-Regular.ttf')}
 
   html,
   body {
